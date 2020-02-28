16ded73c98cfe3fb6a34787a15591d21f029495e
Yann Xiaoyan Yang <yannxyang@sina.com>
2020-02-28 16:50:41 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,20 +1,16 @@
 function basicTeenager(age)
 {
-if(age>=13&&age<=19)
-    {
-    print("You are a teenager!")
+if(age>=13 && age<=19){
+    return "You are a teenager!";
     }
 }
 
-function teenager(age)
-{
-  if(age>=13&&age<=19)
-    {
-      print("You are a teenager!")
+function teenager(age){
+  if(age>=13&&age<=19){
+      return "You are a teenager!";
     }
-else
-    {
-    print("You are a teenager!")
+else {
+    return "You are a teenager!";
     }
 }
 
