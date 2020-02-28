d8791fb23183c20bf418ffc6c99e8885ba153520
Yann Xiaoyan Yang <yannxyang@sina.com>
2020-02-28 17:00:34 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -14,7 +14,14 @@ else {
     }
 }
 
-
+function ageChecker(age) {
+  if(age>=13&&age<=19){
+      return "You are a teenager!";
+    }else if （age<=12){
+      return "You are a kid";
+    }else {
+    return "You are a grownup";
+    }
 
 }
 
